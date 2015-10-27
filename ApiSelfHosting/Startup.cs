0e3de1928757cb766fc8bd465690d94ad5d9e4fd
jabiel <jabiel@o2.pl>
2015-10-27 09:29:24 +0100

    Examples

---
+++ ApiSelfHosting/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(ApiSelfHosting.Startup))]
+
+namespace ApiSelfHosting
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+#if DEBUG
+            app.UseErrorPage();
+#endif
+
+
+            app.UseWelcomePage(new Microsoft.Owin.Diagnostics.WelcomePageOptions()
+            {
+                Path = new PathString("/welcome")
+            });
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html";
+
+                string output = string.Format(
+                    "<p>I'm running on {0} </p><p>From assembly {1}</p>",
+                    Environment.OSVersion,
+                    System.Reflection.Assembly.GetEntryAssembly().FullName
+                    );
+
+                return context.Response.WriteAsync(output);
+
+            });
+        }
+    }
+}
---
+++ SerialPortTest/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialPortTest
+{
+    /// <summary>
+    /// Comunicatation between rpi and arduino connected threw usb cable
+    /// </summary>
+    class Program
+    {
+        static SerialPort sp;
+        public static void Main(string[] args)
+        {
+            sp = new SerialPort("/dev/ttyUSB0", 9600, Parity.None, 8, StopBits.One);
+            sp.Open();
+
+            Thread.Sleep(200);
+
+            Console.WriteLine(">" + ReadLine());
+
+            for (var i = 0; i < 3; i++)
+            {
+                string result = string.Format("Test {0}", i);
+                sp.WriteLine(result);
+                Console.WriteLine("<" + result);
+
+
+                Thread.Sleep(200);
+
+                var ss = ReadLine();
+                Console.WriteLine(">" + ss);
+            }
+
+            sp.Close();
+        }
+
+        public static string ReadLine()
+        {
+            string rxString = "";
+            byte prevb = 0;
+            var b = (byte)sp.ReadByte();
+
+            while (b != 10 || prevb != 13) // 1310 EOL
+            {
+                //Console.WriteLine(":"+b+" " + (char)b);
+                rxString += ((char)b);
+                prevb = b;
+                b = (byte)sp.ReadByte();
+            }
+
+            return rxString;
+        }
+
+
+    }
+}
